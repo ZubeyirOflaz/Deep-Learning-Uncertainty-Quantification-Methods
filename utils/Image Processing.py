a7a5437554a1240ab7858fb7890a197be6b22ad3
ZubeyirOflaz <zubeyir.oflaz@gmail.com>
2022-06-08 02:00:35 +0200

    Finalization of Image Processing.py and addition of some files to .gitignore

--- utils/Image Processing.py
+++ utils/Image Processing.py
@@ -2,8 +2,8 @@ import cv2
 import numpy as np
 import os
 import logging
-from skimage.filters import gaussian
 import random
+from  pathlib import Path
 
 '''This script is used in order to apply motion and gaussian blur to the casting dataset in order. The last function,
 create_blur_database receives the directory of images, the desired ratio of motion blurred images, motion blur kernel 
@@ -12,39 +12,56 @@ images in a given directors and saves it to a new folder
 '''
 
 
-def apply_motion_blur(image_dir, kernel_size):
+def apply_motion_blur(image_dir, final_dir, kernel_size=10):
     img = cv2.imread(image_dir)
-    file_name, file_extension = os.path.splitext(image_dir)
+    file_path, file_extension = os.path.splitext(image_dir)
+    file_name = Path(image_dir).stem
     # Specify the kernel size.
     # The greater the size, the more the motion.
     kernel = np.zeros((kernel_size, kernel_size))
     kernel[int((kernel_size - 1) / 2), :] = np.ones(kernel_size)
     kernel /= kernel_size
     motion_blurred = cv2.filter2D(img, -1, kernel)
-    file_destination = 'modified/' + file_name + '_mb' + file_extension
+    file_destination = final_dir + '\\' + file_name + '_mb' + file_extension
     cv2.imwrite(file_destination, motion_blurred)
     logging.debug(f'{file_name} has been modified with motion blur')
 
 
-def apply_gaussian_blur(image_dir, filter_sigma):
+def apply_gaussian_blur(image_dir,final_dir, filter_sigma=2):
     img = cv2.imread(image_dir)
-    file_name, file_extension = os.path.splitext(image_dir)
-    # Specify the kernel size.
-    # The greater the size, the more the motion.
-    blurred_image = gaussian(img, sigma=(filter_sigma, filter_sigma))
-    file_destination = 'modified/' + file_name + '_gaussian' + file_extension
-    cv2.imwrite(file_destination, blurred_image)
+    file_path, file_extension = os.path.splitext(image_dir)
+    file_name = Path(image_dir).stem
+    '''cv2.imshow('image', img);
+    cv2.waitKey(0);
+    cv2.destroyAllWindows();
+    cv2.waitKey(1)
+'''
+    blurred_image = cv2.GaussianBlur(img, (5,5), 2)
+    file_destination = final_dir + '\\' + file_name + '_gaussian' + file_extension
+    if not cv2.imwrite(file_destination, blurred_image):
+        logging.error('Could not write the image')
     logging.debug(f'{file_name} has been modified with gaussian blur')
 
 
-def create_blur_database(directory_location, mb_gaussian_ratio=0.9, mb_kernel=20, gaussian_sigma=1):
+def create_blur_database(directory_location,final_dir, mb_gaussian_ratio=0.75):
     image_files = os.listdir(directory_location)
+    image_files = [directory_location + '\\' + x for x in image_files]
     random.shuffle(image_files)
-    separation_point = int(len(image_files) / mb_gaussian_ratio)
+    separation_point = int(len(image_files) * mb_gaussian_ratio)
     logging.debug(
         f'{str(separation_point)} will be applied motion blur and {str((len(image_files) - separation_point))} will '
         f'be applied gaussian blur')
     motion_blur_list = image_files[:separation_point]
     gaussian_list = image_files[separation_point:]
-    map(apply_motion_blur, motion_blur_list)
-    map(apply_gaussian_blur, gaussian_list)
+    for i in motion_blur_list:
+        apply_motion_blur(i, final_dir)
+    for i in gaussian_list:
+        apply_gaussian_blur(i, final_dir)
+    print(len(gaussian_list))
+    print(separation_point)
+    print(len(image_files))
+
+casting_train_ok = r'N:\Thesis\Datasets\casting_dataset\casting_data\casting_data\train\ok_front'
+final_dir = r'N:\Thesis\modified\train\ok_front'
+create_blur_database(casting_train_ok, final_dir)
+
