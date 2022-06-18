d3cba3c4cf180a1a75efe48e411a57878ab889ba
ZubeyirOflaz <zubeyir.oflaz@gmail.com>
2022-06-18 12:39:08 +0200

    Refinement for the working model for optuna CNN script

--- optuna_cnn_model.py
+++ optuna_cnn_model.py
@@ -8,6 +8,8 @@ from torch.utils.data import DataLoader
 from torch.utils.data import Dataset, random_split
 import pickle
 import helper
+import logging
+import time
 
 import optuna
 from optuna.trial import TrialState
@@ -22,29 +24,32 @@ from config import casting_args as args
 train_set_path = args['train_set_path']
 test_set_path = args['test_set_path']
 
-batch_size = 32
+batch_size = 128
 image_resolution = 127
+num_workers = 0
 
 LOG_INTERVAL = 10
-EPOCHS = 100
+EPOCHS = 25
 
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda:0" if use_cuda else "cpu")
+print(device)
+print(torch.cuda.device_count())
 torch.backends.cudnn.benchmark = True
 
-transformations = transforms.Compose([transforms.Resize(image_resolution),
-                                      # transforms.RandomCrop(127),
+transformations = transforms.Compose([transforms.Resize(int((image_resolution+1)/2)*3),
+                                      transforms.RandomCrop(image_resolution),
                                       transforms.Grayscale(),
                                       transforms.ToTensor(),
                                       transforms.Normalize(0.5, 0.5)])
 
 train_set = datasets.ImageFolder(train_set_path, transform=transformations)
-train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True)
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers= num_workers, pin_memory=True)
 # images, labels = next(iter(train_loader))
 # helper.imshow(images[0], normalize=False)
 
 test_set = datasets.ImageFolder(test_set_path, transform=transformations)
-test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=True, num_workers= num_workers)
 
 N_TRAIN_EXAMPLES = len(train_set)
 N_TEST_EXAMPLES = len(test_set)
@@ -52,7 +57,7 @@ N_TEST_EXAMPLES = len(test_set)
 
 def optuna_model(trial):
     layers = []
-    num_cnn_blocks = trial.suggest_int('num_cnn_blocks', 2, 4)
+    num_cnn_blocks = trial.suggest_int('num_cnn_blocks', 1, 3)
     num_dense_nodes = trial.suggest_categorical('num_dense_nodes',
                                                 [64, 128, 512, 1024])
     dense_nodes_divisor = trial.suggest_categorical('dense_nodes_divisor',
@@ -75,9 +80,11 @@ def optuna_model(trial):
         #    input_channels = layers[-2].out_channels
         layers.append(nn.Conv2d(input_channels, num_filters, kernel_size))
         input_channels = num_filters
-        layers.append(nn.MaxPool2d(4, 2))
+        layers.append(nn.MaxPool2d(4, stride=2))
     layers.append(nn.Conv2d(input_channels, 64, kernel_size))
-    linear_input = layers[-1].out_channels * layers[-1].kernel_size[0] * layers[-1].kernel_size[1]
+    layers.append(nn.AdaptiveMaxPool2d((12, 12)))
+    layers.append(nn.Flatten())
+    linear_input = 64 * 12 * 12
     layers.append(nn.Linear(linear_input, dict_params['num_dense_nodes']))
     layers.append(nn.ReLU())
     layers.append(nn.Dropout(dict_params['drop_out']))
@@ -85,14 +92,16 @@ def optuna_model(trial):
                             int(dict_params['num_dense_nodes'] / dict_params['dense_nodes_divisor'])))
     layers.append(nn.ReLU())
     layers.append(nn.Dropout(dict_params['drop_out']))
-    layers.append(nn.Linear(layers[-3].out_features, 2))
+    layers.append(nn.Linear(int(dict_params['num_dense_nodes'] / dict_params['dense_nodes_divisor']), 2))
     layers.append(nn.LogSoftmax(dim=1))
-
     return nn.Sequential(*layers)
+
 
 def objective(trial):
     # Generate the model.
     model = optuna_model(trial).to(device)
+    #print(model)
+    logging.error('model has been compiled')
 
     # Generate the optimizers.
     optimizer_name = trial.suggest_categorical("optimizer", ["Adam", "RMSprop", "SGD"])
@@ -101,20 +110,23 @@ def objective(trial):
 
     # Training of the model.
     for epoch in range(EPOCHS):
+        t0 = time.time()
+        #logging.error('training has started')
         model.train()
         for batch_idx, (data, target) in enumerate(train_loader):
             # Limiting training data for faster epochs.
             if batch_idx * batch_size >= N_TRAIN_EXAMPLES:
                 break
 
-            data, target = data.view(data.size(0), -1).to(device), target.to(device)
+            data, target = data.to(device, non_blocking=True), target.to(device, non_blocking=True)
 
             optimizer.zero_grad()
             output = model(data)
+            #logging.error('forward pass has been completed')
             loss = F.nll_loss(output, target)
             loss.backward()
             optimizer.step()
-
+        print(f'Seconds between epoch:{time.time()-t0}')
         # Validation of the model.
         model.eval()
         correct = 0
@@ -123,29 +135,31 @@ def objective(trial):
                 # Limiting validation data.
                 if batch_idx * batch_size >= N_TEST_EXAMPLES:
                     break
-                data, target = data.view(data.size(0), -1).to(device), target.to(device)
+                data, target = data.to(device, non_blocking=True), target.to(device, non_blocking=True)
                 output = model(data)
                 # Get the index of the max log-probability.
                 pred = output.argmax(dim=1, keepdim=True)
                 correct += pred.eq(target.view_as(pred)).sum().item()
 
         accuracy = correct / min(len(test_loader.dataset), N_TEST_EXAMPLES)
+        print(accuracy)
 
         trial.report(accuracy, epoch)
-
-        # with open("{}.pickle".format(trial.number), "wb") as fout:
-        #  pickle.dump(model, fout)
-
-        # Handle pruning based on the intermediate value.
         if trial.should_prune():
             raise optuna.exceptions.TrialPruned()
+
+    with open("model_repo\\{}.pickle".format(trial.number), "wb") as fout:
+        pickle.dump(model, fout)
+
+        # Handle pruning based on the intermediate value.
+
 
     return accuracy
 
 
 if __name__ == "__main__":
     study = optuna.create_study(direction="maximize")
-    study.optimize(objective, n_trials=100, timeout=60000)
+    study.optimize(objective, n_trials=25, timeout=60000)
 
     pruned_trials = study.get_trials(deepcopy=False, states=[TrialState.PRUNED])
     complete_trials = study.get_trials(deepcopy=False, states=[TrialState.COMPLETE])
@@ -163,3 +177,8 @@ if __name__ == "__main__":
     print("  Params: ")
     for key, value in trial.params.items():
         print("    {}: {}".format(key, value))
+
+with open("model_repo\\{}.pickle".format(study.best_trial.number), "rb") as fin:
+    best_result = pickle.load(fin)
+with open(f'model_repo\\best_models\\{study.best_trial.value}.pickle', 'wb') as fout:
+    pickle.dump(best_result, fout)
