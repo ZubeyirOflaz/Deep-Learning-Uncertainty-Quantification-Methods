6b23b6ec6fa72c723f96ee4059b9eff5c1dee90c
ZubeyirOflaz <zubeyir.oflaz@gmail.com>
2022-07-05 18:18:14 +0200

    Structure change and refinement of laplace_approximation_arrhythmia.py

--- laplace_approximation_arrhythmia.py
+++ laplace_approximation_arrhythmia.py
@@ -71,7 +71,7 @@ train_x = arrhythmia_train[:, 0]
 train_y = arrhythmia_train[:, 1]
 
 train_x = torch.tensor([i.numpy() for i in train_x]).to(torch.device('cuda'))
-train_y = torch.tensor([i.numpy() for i in train_y]).to(torch.device('cuda'))
+train_y = torch.tensor([i.numpy() for i in train_y]).to(torch.device('cuda')).flatten()
 train = TensorDataset(train_x, train_y)
 train_loader = DataLoader(train, shuffle=False, **params)
 
@@ -79,16 +79,38 @@ test_x = arrhythmia_test[:,0]
 test_y = arrhythmia_test[:,1]
 
 test_x = torch.tensor([i.numpy() for i in test_x]).to(torch.device('cuda'))
-test_y = torch.tensor([i.numpy() for i in test_y]).to(torch.device('cuda'))
+test_y = torch.tensor([i.numpy() for i in test_y]).to(torch.device('cuda')).flatten()
 test = TensorDataset(test_x, test_y)
 
 test_loader = DataLoader(test, shuffle=False, **params)
 
 arrhythmia_model = arrhythmia_model.to(torch.device('cuda'))
-targets = torch.cat([y for x, y in train_loader], dim=0).cpu()
+targets = torch.cat([y for x, y in test_loader], dim=0).cpu()
 targets = torch.reshape(targets,(-1,))
 
-probs_map = predict(train_loader, arrhythmia_model)
+probs_map = predict(test_loader, arrhythmia_model)
 acc_map = (probs_map.argmax(-1) == targets).float().mean()
+nll_map = -dists.Categorical(probs_map).log_prob(targets).mean()
+print(f'[MAP] Acc.: {acc_map:.1%}; NLL: {nll_map:.3}')
 
-check_accuracy(test_loader,arrhythmia_model)
+torch.cuda.empty_cache()
+
+subnetwork_mask = ModuleNameSubnetMask(arrhythmia_model, module_names=['6', '9'])
+subnetwork_mask.select()
+subnetwork_indices = subnetwork_mask.indices
+
+subnetwork_indices = torch.LongTensor(subnetwork_indices.cpu())
+
+la = Laplace(arrhythmia_model, likelihood='classification', subset_of_weights='subnetwork',
+             hessian_structure='full', subnetwork_indices=subnetwork_indices)
+la.fit(train_loader)
+
+pred = predict(test_loader, la, laplace=True)
+
+probs_laplace = predict(test_loader, la, laplace=True)
+acc_laplace = (probs_laplace.argmax(-1) == targets).float().mean()
+# ece_laplace = ECE(bins=15).measure(probs_laplace.numpy(), targets.numpy())
+nll_laplace = -dists.Categorical(probs_laplace).log_prob(targets).mean()
+
+print(f'[Laplace] Acc.: {acc_laplace:.1%} NLL: {nll_laplace:.3}')
+
